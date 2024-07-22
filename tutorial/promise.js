9c7b41d873874458677a0d831e84905aaed21240
mariuszpiela <mariusz.piela@tmsolution.pl>
2024-07-22 11:11:14 +0200

    fetch correct

--- tutorial/promise.js
+++ tutorial/promise.js
@@ -8,13 +8,16 @@ async function main() {
     const url = 'https://jsonplaceholder.typicode.com/users';
     //asynchroniczne wywołanie
      fetch(url)
-        .then(response => {
+        .then(async response => {
             // Sprawdzenie, czy odpowiedź jest poprawna (status 200-299)
             if (!response.ok) {
                 throw new Error(`HTTP error! Status: ${response.status}`);
             }
             // Parsowanie odpowiedzi jako JSON
-            return response.json();
+            const data =await response.json()
+            console.log(data);   
+            
+            
         })
         .catch(error => {
             // Obsługa błędów
